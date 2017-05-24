fbc100b3ff0f09bf360357edb12af621ab37a345
Sergey Zhuravlev <zhurs@ya.ru>
2017-05-24 11:45:02 +0300

    added shade plugin

--- src/main/java/krok/Cli.java
+++ src/main/java/krok/Cli.java
@@ -4,9 +4,15 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -16,6 +22,7 @@ import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 
 import com.beust.jcommander.JCommander;
+import org.apache.commons.io.FileUtils;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
@@ -30,13 +37,17 @@ public class Cli {
     private static final Logger logger = LoggerFactory.getLogger(Cli.class);
 
     private static final String FOP_CONF_XML = "fop.conf.xml";
+    public static final String[] MEDIA_FILES = {
+            "logo_big.gif"
+    };
+
     private final CliParameters params;
 
     private Cli(CliParameters params) {
         this.params = params;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         CliParameters params = new CliParameters();
         JCommander.newBuilder()
                 .addObject(params)
@@ -45,7 +56,7 @@ public class Cli {
         new Cli(params).run();
     }
 
-    private void run() {
+    private void run() throws IOException {
         KrokData krokData = new KrokGenerator(params.pages, params.number, params.seed)
                 .generate();
 
@@ -78,28 +89,54 @@ public class Cli {
         return stringWriter.toString();
     }
 
-    private void makePdf(String xml) {
-        File conf = new File(KrokGenerator.class.getClassLoader().getResource(FOP_CONF_XML).getFile());
+    private void makePdf(String xml) throws IOException {
+        Path tempDir = makeWorkingDirectory();
 
+        try (
+                ByteArrayInputStream xmlStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+                InputStream confStream = getResource(FOP_CONF_XML).openStream();
+                OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(params.outputFile)));
+        )
+        {
+            Fop fop = FopFactory.newInstance(tempDir.toUri(), confStream)
+                    .newFop(MimeConstants.MIME_PDF, out);
+            Transformer transformer = TransformerFactory.newInstance()
+                    .newTransformer();
+
+            Source src = new StreamSource(xmlStream);
+            Result res = new SAXResult(fop.getDefaultHandler());
+
+            transformer.transform(src, res);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            FileUtils.deleteDirectory(tempDir.toFile());
+        }
+    }
+
+    private URL getResource(String path) {
+        URL url = getClass().getClassLoader().getResource(path);
+        if (url == null) {
+            throw new IllegalArgumentException("Resource " + path + " not found");
+        }
+        return url;
+    }
+
+    private Path makeWorkingDirectory() {
         try {
-            FopFactory fopFactory = FopFactory.newInstance(conf);
-
-            try (
-                    OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(params.outputFile)))
-            )
-            {
-                Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
-
-                TransformerFactory factory = TransformerFactory.newInstance();
-                Transformer transformer = factory.newTransformer(); // identity transformer
-
-                //File fo = new File(KrokGenerator.class.getClassLoader().getResource("template.xml").getFile());
-                Source src = new StreamSource(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)));
-
-                Result res = new SAXResult(fop.getDefaultHandler());
-
-                transformer.transform(src, res);
+            Path tempDir = Files.createTempDirectory("krok-gen");
+            logger.info("temporary directory: {}", tempDir);
+            try {
+                for (String mediaFile : MEDIA_FILES) {
+                    try (InputStream inputStream = getResource(mediaFile).openStream()) {
+                        Files.copy(inputStream, tempDir.resolve(mediaFile));
+                    }
+                }
+            } catch (Exception e) {
+                FileUtils.deleteDirectory(tempDir.toFile());
+                throw e;
             }
+            return tempDir;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
